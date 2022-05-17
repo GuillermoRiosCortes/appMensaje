05ea886289efbffaa5e49345c1616ac6da9ffdf9
GuillermoRiosCortes <guille.rios.cortes96@gmail.com>
2022-05-17 15:06:14 -0400

    Creacion aplicacion python

---
+++ aplicaciones/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def mensaje():
+    return jsonify({"response":"Bienvenidos Technology Latam"})
+
+@app.route('/error', methods=['GET'])
+def err():
+    error = "Error! este es un mensaje de error"
+    return error
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=4000, debug=True)
